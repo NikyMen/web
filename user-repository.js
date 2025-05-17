e72ba970d0c47065c0c8d76f2073315b3c334b37
Nicolas <niko.moto@hotmail.com.ar>
2025-05-16 23:08:34 -0300

    agregue un carpeta y actualice bcryp

---
+++ user-repository.js
@@ -0,0 +1,81 @@
+// user-repository.js
+import DBLocal from "db-local"  // guarda los datos en archivos locales
+import crypto from "crypto"     // genera UUID para los IDs
+import bcrypt from "bcryptjs"   // encripta y compara contraseñas
+import {SALT_ROUNDS} from "./config.js"
+
+// Inicializa la DB
+const { Schema } = new DBLocal({ path: "./db" })
+
+// Define el modelo User
+const User = Schema("User", {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+// Clase con métodos para manejar usuarios
+export class UserRepository {
+  static async create({ username, password }) {
+    Validation.username(username)
+    Validation.password(password)
+
+    // Verifica si ya existe el usuario
+    if (User.findOne({ username })) {
+      throw new Error("Username already exists")
+    }
+
+    const id = crypto.randomUUID()
+    const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS)
+
+    // Crea y guarda el nuevo usuario
+    User.create({
+      _id: id,
+      username,
+      password: hashedPassword
+    }).save()
+
+    return id
+    
+    
+  }
+  
+  // Método para iniciar sesión
+  // Verifica si el usuario y la contraseña son válidos
+  static async login({ username, password }) {
+    
+    Validation.username(username)
+    Validation.password(password)
+
+    const user = User.findOne({ username })
+    if (!user) throw new Error("username does not exist")
+
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) throw new Error("password is invalid")
+
+    const { password: _, ...publicUser } = user
+
+    return publicUser
+  }
+}
+
+// Clase de validaciones reutilizable
+class Validation {
+  static username(username) {
+    if (typeof username !== 'string') {
+      throw new Error('username must be a string')
+    }
+    if (username.length < 3) {
+      throw new Error('username must be at least 3 characters long')
+    }
+  }
+
+  static password(password) {
+    if (typeof password !== 'string') {
+      throw new Error('password must be a string')
+    }
+    if (password.length < 6) {
+      throw new Error('password must be at least 6 characters long')
+    }
+  }
+}
