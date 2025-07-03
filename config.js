920c8699e7978882b5296f8637052f83d8f0775d
Nicolas <niko.moto@hotmail.com.ar>
2025-07-02 22:52:45 -0300

    fix : puerto

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 export const {
-    PORT = 3000,
     SALT_ROUNDS = 10,
     SECRET_JWT_KEY = "hola-esta-tiene-que-ser-una-contraseña-muy-muy-pero-muy-larga-para-que-sea-muy-segura-y-nnadie-pueda-hackearme"
 } = process.env
